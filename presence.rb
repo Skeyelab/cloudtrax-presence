0d65fc8cc1d14a82ad69d4db2def3f34ce856ad7
Eric Dahl <eric@skeyelab.com>
2016-09-09 10:06:00 -0500

    cleanup

--- presence.rb
+++ presence.rb
@@ -13,11 +13,6 @@ configure do
   set :mongo_db, db[:pings]
 end
 
-get '/collections/?' do
-  content_type :json
-  settings.mongo_db.database.collection_names.to_json
-end
-
 helpers do
   # a helper method to turn a string ID
   # representation into a BSON::ObjectId
@@ -40,61 +35,34 @@ helpers do
   end
 end
 
+get '/' do
+  "Set your Cloudtrax Presence Reporting Server Location to: <br/>#{request.base_url}/ping"
+end
+
+get '/collections/?' do
+  content_type :json
+  settings.mongo_db.database.collection_names.to_json
+end
+
 # list all documents in the test collection
-get '/documents/?' do
+get '/pings/?' do
   content_type :json
   settings.mongo_db.find.to_a.to_json
 end
 
 # find a document by its ID
-get '/document/:id/?' do
+get '/pings/:id/?' do
   content_type :json
   document_by_id(params[:id])
 end
 
 # insert a new document from the request parameters,
 # then return the full document
-post '/new_document/?' do
+post '/ping/?' do
   params = JSON.parse request.body.read
   params[:timestamp] = Time.now.to_i
   content_type :json
   db = settings.mongo_db
   result = db.insert_one params
   db.find(:_id => result.inserted_id).to_a.first.to_json
-end
-
-# update the document specified by :id, setting its
-# contents to params, then return the full document
-put '/update/:id/?' do
-  content_type :json
-  id = object_id(params[:id])
-  settings.mongo_db.find(:_id => id).
-    find_one_and_update('$set' => request.params)
-  document_by_id(id)
-end
-
-# update the document specified by :id, setting just its
-# name attribute to params[:name], then return the full
-# document
-put '/update_name/:id/?' do
-  content_type :json
-  id   = object_id(params[:id])
-  name = params[:name]
-  settings.mongo_db.find(:_id => id).
-    find_one_and_update('$set' => {:name => name})
-  document_by_id(id)
-end
-
-# delete the specified document and return success
-delete '/remove/:id' do
-  content_type :json
-  db = settings.mongo_db
-  id = object_id(params[:id])
-  documents = db.find(:_id => id)
-  if !documents.to_a.first.nil?
-    documents.find_one_and_delete
-    {:success => true}.to_json
-  else
-    {:success => false}.to_json
-  end
 end
